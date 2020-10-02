9feb496c2373085046a2fc96f649606a28153f97
ipranawake <30821059+ipranawake@users.noreply.github.com>
2020-10-02 06:09:13 +0530

    Info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+// ******************************************************************
+// SSD Assignment 02   - Software Engineering 
+// Udayangani Hamy W.C - IT 1602 3574 
+// Ranawake P I        - IT 1609 7520
+// ******************************************************************
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
