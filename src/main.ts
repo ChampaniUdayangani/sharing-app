64ec34436cfe6b5d46ef6364f331bc0cb7736691
ipranawake <30821059+ipranawake@users.noreply.github.com>
2020-10-04 01:16:24 +0530

    main

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -14,5 +13,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));
